1ac8ff36415b7fbb64b3d5ad883f69a7d745df00
Anthony Grieco <83081445+agrieco20@users.noreply.github.com>
2023-09-15 22:20:59 -0400

    Add files via upload

--- Priority Queue/Driver.java
+++ Priority Queue/Driver.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 /**
  * @author: Anthony Grieco
  * @Date: 9/14/2023
@@ -7,6 +5,7 @@ import java.util.ArrayList;
  * Project Description: This project builds a Priority Queue, which can be used to sort any number of elements based on their perceived level of importance. In this particular case, this Priority Queue takes the form of a Max Heap.
  * File Description: The Driver class acts as the test (or driver) program to ensure that the Max Heap structure being implemented actually works.
  */
+
 public class Driver {
     public static void main (String [] args){
 //        System.out.println("Test"); //Test
@@ -18,16 +17,23 @@ public class Driver {
         KeyPair pair1 = new KeyPair(7, 1);
         KeyPair pair2 = new KeyPair(9, 2);
         KeyPair pair3 = new KeyPair(8, 3);
+        KeyPair pair4 = new KeyPair(10, 4);
+        KeyPair pair5 = new KeyPair(9, 5);
+        KeyPair pair6 = new KeyPair(1, 6);
 //        Heap.add(pair);
 //
 //        System.out.println(Heap.get(0).value);
 
-        PriorityQueue Heap1 = new PriorityQueue();
-        Heap1.push(pair1);
-        Heap1.push(pair2);
-        Heap1.push(pair3);
+        PriorityQueue pqueue = new PriorityQueue();
+        pqueue.push(pair1);
+        pqueue.push(pair2);
+        pqueue.push(pair3);
+        pqueue.push(pair4);
+        pqueue.push(pair5);
+        pqueue.push(pair6);
 
-        System.out.println(Heap1.pop());
+//        System.out.println("---");
+        System.out.println(pqueue.pop());
 
     }
 }
--- Priority Queue/PriorityQueue.java
+++ Priority Queue/PriorityQueue.java
@@ -13,8 +13,10 @@ public class PriorityQueue {
     static KeyPair keyNode; //Used to represent the current/newest node added to the "Heap". The Key of the keyNode will be compared to the Key of its parent node to see if they should be swapped (based on which element has the higher Key)
     static KeyPair parent; //Used to represent the Parent node of the keyNode (so that their Keys can be compared and their placements swapped if the keyNode's key is higher)
     static KeyPair root; //Indicates which node is at the top of the "Heap" (has the highest Key value and will be the node that is Popped and whose value will subsequently be returned). It is also stored at the first index of the "Heap" Arraylist
-    static KeyPair tempNode; //Acts as a temporary variable so that the "keyNode" and "parent" nodes can be swapped if "keyNode" has a higher Key than its "parent" does.
-    static KeyPair result; //Used to store the node that was previously the "root" before being popped from the "Heap" and the reorganization of the structure that follows just after
+    static int tempNodeKey; //Key: Acts as a temporary variable so that the "keyNode" and "parent" nodes can be swapped if "keyNode" has a higher Key than its "parent" does.
+    static int tempNodeValue; //Value: Acts as a temporary variable so that the "keyNode" and "parent" nodes can be swapped if "keyNode" has a higher Key than its "parent" does.
+
+    static int result; //Used to store the value of the previous "root" node before being popped from the "Heap" and the reorganization of the structure that follows just after
     static int p_idx; //Used to keep track of which index in the arraylist the "Heap" is currently sorting through in order to determine the new parent-child structure once a "Pop" has been initialized and the original "root" has been removed
     static KeyPair leftChild; //Used by the "Pop" method to determine whether the current child nodes should in fact be the parent node. If not, the "leftChild" is stored in the "Heap" arraylist at the '(2 * "p_idx") + 1' index where "p_idx" is the current index of the parent node being checked
     static KeyPair rightChild; //Used by the "Pop" method to determine whether the current child nodes should in fact be the parent node. If not, the "rightChild" is stored in the "Heap" arraylist at the '(2 * "p_idx") + 2' index where "p_idx" is the current index of the parent node being checked
@@ -38,22 +40,26 @@ public class PriorityQueue {
             parent = Heap.get(((Heap.size() - 1) -1) / 2);
         }
 
+        //Checks to see if the newest node has a greater key than its parent (and if it does the two are swapped)
         while (keyNode != root && keyNode.key > parent.key) {
-            tempNode = parent;
+            tempNodeKey = parent.key;
+            tempNodeValue = parent.value;
 
-            parent = keyNode;
+            parent.key = keyNode.key;
+            parent.value = keyNode.value;
             Heap.get(((Heap.size() - 1) -1)/2).key = keyNode.key;
             Heap.get(((Heap.size() - 1) -1)/2).value = keyNode.value;
 
-            keyNode = tempNode;
-            Heap.get(Heap.size() - 1).key = tempNode.key;
-            Heap.get(Heap.size() - 1).value = tempNode.value;
+            keyNode.key = tempNodeKey;
+            keyNode.value = tempNodeValue;
+            Heap.get(Heap.size() - 1).key = tempNodeKey;
+            Heap.get(Heap.size() - 1).value = tempNodeValue;
         }
     }
 
     //The Pop Method is responsible for removing the "root" (first index) from the "Heap" and then reorganizing the "Heap" structure so that the remaining node with the next highest key value becomes the new "root"
     static int pop(){
-        result = root;
+        result = root.value;
 
         Heap.get(0).key = keyNode.key;
         Heap.get(0).value = keyNode.value;
@@ -68,32 +74,33 @@ public class PriorityQueue {
         for (int i = 0; i < Heap.size(); i++){
             System.out.println(Heap.get(i).value);
         }
+        System.out.println("---");
 
         //------
 
-        return result.value; //Heap.get(0).value;
+        return result; //Heap.get(0).value;
     }
 
     //After being called by the "Pop" Method, the "Heapify" method is responsible for reorganizing the remaining nodes after the "root" has been removed to determine which of them has the highest Key value
     private static void Heapify(int p_idx){
-//        NEED TO FIND A WAY TO PREVENT THE INDEX OUT OF BOUNDS ERROR (try to implement a "Try-Catch" experession before the 4 "leftChild" and "rightChild" statements below)
-// *****FOR WHATEVER REASON THE PROGRAM DOESN'T RECOGNIZE "leftChild" or "rightChild" and instead throws an "NullPointerException" (cannot use try-catch to ignore it...need to fix this)
+//        NEED TO FIND A WAY TO PREVENT THE INDEX OUT OF BOUNDS ERROR (try to implement a "Try-Catch" expression before the 4 "leftChild" and "rightChild" statements below)
+//CORRECTED: *****FOR WHATEVER REASON THE PROGRAM DOESN'T RECOGNIZE "leftChild" or "rightChild" and instead throws an "NullPointerException" (cannot use try-catch to ignore it...need to fix this)
 
-//        try {
-            leftChild.key = Heap.get((2 * p_idx) + 1).key;
-            leftChild.value = Heap.get((2 * p_idx) + 1).value;
-//        }
-//        catch(NullPointerException e){
-//            return;
-//        }
+        try {
+            leftChild = Heap.get((2 * p_idx) + 1); //.key
+            leftChild = Heap.get((2 * p_idx) + 1); //.value
+        }
+        catch(IndexOutOfBoundsException e){
+            return;
+        }
 
-//        try {
-            rightChild.key = Heap.get((2 * p_idx) + 2).key;
-            rightChild.value = Heap.get((2 * p_idx) + 2).value;
-//        }
-//        catch(NullPointerException e){
-//            return;
-//        }
+        try {
+            rightChild = Heap.get((2 * p_idx) + 2);
+            rightChild = Heap.get((2 * p_idx) + 2);
+        }
+        catch(IndexOutOfBoundsException e){
+            return; //NEED TO CHANGE THIS TO A BOOLEAN
+        }
         //---
 
         largest = Heap.get(p_idx);
@@ -110,24 +117,25 @@ public class PriorityQueue {
             return;
         }
         else{
-            tempNode = parent;
+            tempNodeKey = parent.key;
+            tempNodeValue = parent.value;
 
             if (largest == leftChild){
                 Heap.get(p_idx).key = leftChild.key;
                 Heap.get(p_idx).value = leftChild.value;
 
-                Heap.get((2 * p_idx) + 1).key = tempNode.key;
-                Heap.get((2 * p_idx) + 1).value = tempNode.value;
+                Heap.get((2 * p_idx) + 1).key = tempNodeKey;
+                Heap.get((2 * p_idx) + 1).value = tempNodeValue;
             }
 
             else{ //largest == rightChild
                 Heap.get(p_idx).key = rightChild.key;
                 Heap.get(p_idx).value = rightChild.value;
 
-                Heap.get((2 * p_idx) + 2).key = tempNode.key;
-                Heap.get((2 * p_idx) + 2).value = tempNode.value;
+                Heap.get((2 * p_idx) + 2).key = tempNodeKey;
+                Heap.get((2 * p_idx) + 2).value = tempNodeValue;
             }
-            Heapify(p_idx++);
+            Heapify(++p_idx);
         }
     }
 
