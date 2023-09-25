8974bd13d63901f00f6b74f703f300f896137028
Anthony Grieco <83081445+agrieco20@users.noreply.github.com>
2023-09-24 20:23:08 -0400

    Add files via upload

--- Priority Queue/KeyPair.java
+++ Priority Queue/KeyPair.java
@@ -3,7 +3,7 @@
  * @Date: 9/14/2023
  *
  * Project Description: This project builds a Priority Queue, which can be used to sort any number of elements based on their perceived level of importance. In this particular case, this Priority Queue takes the form of a Max Heap.
- * File Description: The Pair class acts as the object that the Max Heap structure will use in order to sort the elements that it's responsible for based on their given importance.
+ * File Description: The KeyPair class acts as the object that the Max Heap structure will use in order to sort the elements that it's responsible for based on their given importance.
  */
 
 public class KeyPair {
