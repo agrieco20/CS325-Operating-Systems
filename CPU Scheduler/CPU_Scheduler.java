3455bf389b62cf3396c0f6aba8312ad29f61a77e
Anthony Grieco <83081445+agrieco20@users.noreply.github.com>
2023-09-28 23:49:19 -0400

    Add files via upload

--- CPU Scheduler/CPU_Scheduler.java
+++ CPU Scheduler/CPU_Scheduler.java
@@ -10,11 +10,14 @@
 
 package com.sos.os;
 
+import static java.lang.Math.pow;
+
 public class CPU_Scheduler implements ProcessScheduler {
     private final PriorityQueue pqueuePrimary = new PriorityQueue(); //Acts as the initial priority queue that new processes are added to in order to be scheduled and later processed by the SAC-SimOS
     private final PriorityQueue pqueueSecondary = new PriorityQueue(); //Acts as priority queue that new processes are initially switched into once they've been processed for 5 CPU Bursts at a time. Once a process has gone through another 5 CPU Bursts once in this queue, they will be swapped back to pqueuePrimary
 
-    private int CPU_BurstCounter = 10; //Used in order to only allocate so many CPU Bursts to each process before moving onto the next process in order to ensure that this algorithm is preemptive and therefore further minimize the average amount of time that jobs are just sitting idly in one of the two queues
+    private int burstRate = 10; //Used so that after processes with lesser waiting times have been processed, the number of CPU cycles allocated to a given process at a single time is multiplied by 10 so that later processes with longer estimated wait times in the queue will be processed faster in order to prevent the required usage of extra resources from being wasted in order to merely switch these processes between queues (meaning less waiting time and memory overhead for the SAC-SimOS CPU)
+    private int CPU_BurstCounter = burstRate; //Used in order to only allocate so many CPU Bursts to each process before moving onto the next process in order to ensure that this algorithm is preemptive and therefore further minimize the average amount of time that jobs are just sitting idly in one of the two queues
     private int remainingBurstCount;
 
     //Used to determine whether processes should be getting moved from Primary to Secondary or vice versa once their CPU Burst allocation has dropped to zero for that cycle
@@ -50,7 +53,7 @@ public class CPU_Scheduler implements ProcessScheduler {
         if (pqSecondaryEmpty == true) {
             while (pqueuePrimary.getHeapSize() > 0 && pqueuePrimary.getHeapItem(0).process.getState() == SimProcessState.TERMINATED) {
                 pqueuePrimary.pop();
-                CPU_BurstCounter = 10;
+                CPU_BurstCounter = burstRate;
             }
             if (CPU_BurstCounter == 0) {
                 queueSwap = new KeyPair(pqueuePrimary.getHeapItem(0).key, pqueuePrimary.getHeapItem(0).process);
@@ -65,28 +68,29 @@ public class CPU_Scheduler implements ProcessScheduler {
                 pqueuePrimary.pop();
 
 //                pqueueSecondary.push(pqueuePrimary.pop());
-                CPU_BurstCounter = 10;
+                CPU_BurstCounter = burstRate;
             }
 
             if (pqueuePrimary.getHeapSize() != 0){
-                if (pqueuePrimary.getHeapItem(0).key != remainingBurstCount){ //New
-                    CPU_BurstCounter = 10; //New
-                } //New
+                if (pqueuePrimary.getHeapItem(0).key != remainingBurstCount){
+                    CPU_BurstCounter = burstRate;
+                }
                 pqueuePrimary.getHeapItem(0).key -= 1;
                 CPU_BurstCounter -= 1;
-                remainingBurstCount = pqueuePrimary.getHeapItem(0).key; //New
+                remainingBurstCount = pqueuePrimary.getHeapItem(0).key;
 
                 p_idReturnValue = pqueuePrimary.getHeapItem(0).process.getPid();
             }
             else{
                 pqPrimaryEmpty = true;
+                burstRate *= 10; //New
             }
         }
 
         if (pqPrimaryEmpty == true) {
             while (pqueueSecondary.getHeapSize() > 0 && pqueueSecondary.getHeapItem(0).process.getState() == SimProcessState.TERMINATED) {
                 pqueueSecondary.pop();
-                CPU_BurstCounter = 10;
+                CPU_BurstCounter = burstRate;
             }
             if (CPU_BurstCounter == 0) {
                 queueSwap = new KeyPair(pqueueSecondary.getHeapItem(0).key, pqueueSecondary.getHeapItem(0).process);
@@ -101,23 +105,24 @@ public class CPU_Scheduler implements ProcessScheduler {
                 pqueueSecondary.pop();
 
 //                pqueueSecondary.push(pqueuePrimary.pop());
-                CPU_BurstCounter = 10;
+                CPU_BurstCounter = burstRate;
             }
 
             if (pqueueSecondary.getHeapSize() != 0){
-                if (pqueueSecondary.getHeapItem(0).key != remainingBurstCount){ //New
-                    CPU_BurstCounter = 10; //New
-                } //New
+                if (pqueueSecondary.getHeapItem(0).key != remainingBurstCount){
+                    CPU_BurstCounter = burstRate;
+                }
 
                 pqueueSecondary.getHeapItem(0).key -= 1;
                 CPU_BurstCounter -= 1;
-                remainingBurstCount = pqueueSecondary.getHeapItem(0).key; //New
+                remainingBurstCount = pqueueSecondary.getHeapItem(0).key;
 
                 p_idReturnValue = pqueueSecondary.getHeapItem(0).process.getPid();
             }
 
             else{
                 pqSecondaryEmpty = true;
+                burstRate *= 10; //New
             }
         }
         return p_idReturnValue;
