d921f56e4f4b62f794e04035f80b4d9967bbc00a
Anthony Grieco <83081445+agrieco20@users.noreply.github.com>
2023-10-31 21:58:37 -0400

    Add files via upload

--- Resource Manager/ResourceManager.java
+++ Resource Manager/ResourceManager.java
@@ -20,25 +20,12 @@ public class ResourceManager implements AccessManager {
 
     //Adds all of the potential resources that a process could access to a resource queue and assigns each a Mutex Lock
     public void addResource(int resource){
-        //Need to:
-        //1. Create a Semaphore class that takes "resource" as a parameter
-        // a. Each resource semaphore is identifiable using its "resource" [just an integer value]
-        // b. Needs a counter to indicate how many processes are looking to access that resource
-        // c. Needs Wait() Method
-        // d. Needs Signal() Method
-        //2. Create an arraylist of the ResourceSemaphores so that processes can later check to see if they can use them in the "requestResource" method
-
         mutexLock mulock = new mutexLock(resource);
         resourceQueue.add(mulock);
     };
 
     //Processes check to see if a resource is currently available (if so, the resource is allocated to that process and locked so that no other process may access it)
     public boolean requestResource(SimProcessInfo process, int resource, boolean write){
-        //Need to:
-        //1. Check to see if the resource the process requesting to access it is actually available (need to use for loop to cycle through the resource arraylist and check for the integer resource value and then to see what the count is. If the count is equal to 0 the resource is available)
-        //2. If resource is available [count = 0], return true
-        //3. Else, update the semaphore object so it calls wait() [decrementing the count] and then return false
-
         boolean requestSuccess = false;
 
         for (int i = 0; i < resourceQueue.size(); i++){
@@ -47,7 +34,6 @@ public class ResourceManager implements AccessManager {
                 return true;
             }
             else if (resource == resourceQueue.get(i).getResourceID() && resourceQueue.get(i).resourceStatus() == false){
-//                Process Busy Wait Queue
                 return false;
             }
         }
@@ -57,10 +43,6 @@ public class ResourceManager implements AccessManager {
 
     //Releases the resource currently being used by a process so that it can be accessed again by another process
     public void releaseResource(SimProcessInfo process, int resource){
-        //Need to:
-        //1. Stop completed processes from being able to access the resource (remove process from semaphore wait queue)
-            //a. Get semaphore object to call signal() and release the resource so that it can then be potentially accessed by other processes again [See textbook for how semaphores work but need to implement that logic here]
-
         for(int i = 0; i < resourceQueue.size(); i++){
             if (resourceQueue.get(i).getResourceID() == resource){
                 resourceQueue.get(i).release();
--- Resource Manager/mutexLock.java
+++ Resource Manager/mutexLock.java
@@ -19,25 +19,28 @@ public class mutexLock {
 
     private boolean available;
 
-    //Sets the unique identifying value of the resource that the Semaphore is limiting access to
+    //Sets the unique identifying value of the resource that the Mutex Lock is limiting access to
     public mutexLock(int resource){
         resourceID = resource;
         available = true;
     }
 
-    //Returns the unique identifying value of the resource the Semaphore is limiting access to
+    //Returns the unique identifying value of the resource the Mutex Lock is limiting access to
     public int getResourceID(){
         return resourceID;
     }
 
+    //Determines whether a resource is currently available
     public boolean resourceStatus(){
         return available;
     }
 
+    //Locks Resource
     public void acquire(){
         available = false;
     }
 
+    //Unlocks Resource
     public void release(){
         available = true;
     }
