e923adb3a0d60e61ca0a0bd56bddb9e40e562868
Anthony Grieco <83081445+agrieco20@users.noreply.github.com>
2023-12-01 20:05:20 -0500

    Add files via upload

--- VirtualMemoryManager/src/com/sos/VirtualMemoryManager.java
+++ VirtualMemoryManager/src/com/sos/VirtualMemoryManager.java
@@ -18,7 +18,9 @@ import com.sos.os.SimProcessInfo;
 import java.util.ArrayList;
 
 public class VirtualMemoryManager implements MemoryManager{
-    ArrayList<FrameTracker> frameVictimTrackerLRU = new ArrayList<>();
+    ArrayList<FrameTracker> frameVictimTrackerLRU_RAM = new ArrayList<>();
+
+    ArrayList<FrameTracker> frameVictimTrackerLRU_HDD = new ArrayList<>();
 
     FrameTracker mostRecentFrame;
     FrameTracker nextPage;
@@ -26,29 +28,58 @@ public class VirtualMemoryManager implements MemoryManager{
     public void writeRequest(SimProcessInfo info, int addr){
         //Check to see if item being requested to be WRITTEN TO is currently in memory */
         if (SimRAM.getInstance().find(info.getPid(), info.getPageNum(addr)) == null){
+            for (int i = 0; i<frameVictimTrackerLRU_HDD.size(); i++){
+                if (frameVictimTrackerLRU_HDD.get(i).getVictimPID() == info.getPid() && frameVictimTrackerLRU_HDD.get(i).getVictimPageNum() == info.getPageNum(addr)){
+                    SimHDD.getInstance().free(frameVictimTrackerLRU_HDD.get(i).getFrame());
+                    frameVictimTrackerLRU_HDD.remove(i);
+                    break;
+                }
+            }
+
             //If page is NOT currently in memory and Page Fault DOESN'T occur, add the given page to the next available frame
             if (SimRAM.getInstance().nextFree() != -1){
                 nextPage = new FrameTracker(SimRAM.getInstance().nextFree());
                 nextPage.setVictimPID(info.getPid());
                 nextPage.setVictimPageNum(info.getPageNum(addr));
 
-                frameVictimTrackerLRU.add(0, nextPage);
+                frameVictimTrackerLRU_RAM.add(0, nextPage);
                 SimRAM.getInstance().store(info.getPage(addr), SimRAM.getInstance().nextFree());
             }
 
             //If page is NOT currently in memory and Page Fault OCCURS, determine which frame will be the victim, check whether the page within that frame has been modified, and then overwrite the frame after writing the changes out to SimHDD as necessary
             else if (SimRAM.getInstance().nextFree() == -1) {
-                FrameTracker victim = new FrameTracker(frameVictimTrackerLRU.get(frameVictimTrackerLRU.size() - 1).getFrame());
+                FrameTracker victim = new FrameTracker(frameVictimTrackerLRU_RAM.get(frameVictimTrackerLRU_RAM.size() - 1).getFrame());
+
+                //Store the frame's contents in specific slot within SimHDD in order to prevent a potentially endless number of blocks being generated in SimHDD
                 if(SimRAM.getInstance().get(victim.getFrame()).isChanged() == true){
-                    SimHDD.getInstance().addStoreBlock(SimRAM.getInstance().get(victim.getFrame()));
+//                    if (SimHDD.getInstance().find(victim.getVictimPID(), victim.getFrame()) == null){
+                    if (SimHDD.getInstance().nextFree() == -1){
+                        SimHDD.getInstance().addBlock();
+
+                        FrameTracker oldVictimFrame = new FrameTracker(SimHDD.getInstance().nextFree());
+                        frameVictimTrackerLRU_HDD.add(oldVictimFrame);
+
+                        SimHDD.getInstance().store(SimRAM.getInstance().get(victim.getFrame()), SimHDD.getInstance().nextFree());
+                    }
+                    else {
+                        FrameTracker oldVictimFrame = new FrameTracker(SimHDD.getInstance().nextFree());
+                        frameVictimTrackerLRU_HDD.add(oldVictimFrame);
+
+                        SimHDD.getInstance().store(SimRAM.getInstance().get(victim.getFrame()), SimHDD.getInstance().nextFree());
+                    }
+//                    }
+//                    else{
+//                    }
+
+////                    SimHDD.getInstance().addStoreBlock(SimRAM.getInstance().get(victim.getFrame()));
                 }
                 SimRAM.getInstance().free(victim.getFrame());
                 SimRAM.getInstance().store(info.getPage(addr), victim.getFrame());
 
                 //Modify page fault frame tracker so that frame being read from will now be the most recently used frame (and last to be chosen for Victim Selection)
-                for(int i = 0; i < frameVictimTrackerLRU.size(); i++) {
-                    if(frameVictimTrackerLRU.get(i).getVictimPID() == victim.getVictimPID() && frameVictimTrackerLRU.get(i).getVictimPageNum() == victim.getVictimPageNum()){
-                        frameVictimTrackerLRU.remove(i);
+                for(int i = 0; i < frameVictimTrackerLRU_RAM.size(); i++) {
+                    if(frameVictimTrackerLRU_RAM.get(i).getVictimPID() == victim.getVictimPID() && frameVictimTrackerLRU_RAM.get(i).getVictimPageNum() == victim.getVictimPageNum()){
+                        frameVictimTrackerLRU_RAM.remove(i);
                         break;
                     }
                 }
@@ -56,22 +87,29 @@ public class VirtualMemoryManager implements MemoryManager{
                 nextPage.setVictimPID(info.getPid());
                 nextPage.setVictimPageNum(info.getPageNum(addr));
 
-                frameVictimTrackerLRU.add(0, nextPage);
+                frameVictimTrackerLRU_RAM.add(0, nextPage);
             }
         }
 
         //If Page IS currently in Memory, find current frame in SimRAM being used and bring it to the top of the "frameVictimTracjerLRU" arraylist so that it won't be immediately selected as the next victim
         else{
-            for (int i = 0; i < frameVictimTrackerLRU.size(); i++){
-                if (frameVictimTrackerLRU.get(i).getVictimPID() == info.getPid() && frameVictimTrackerLRU.get(i).getVictimPageNum() == info.getPageNum(addr)){
-                    mostRecentFrame = new FrameTracker(frameVictimTrackerLRU.get(i).getFrame());
-                    mostRecentFrame.setVictimPID(frameVictimTrackerLRU.get(i).getVictimPID());
-                    mostRecentFrame.setVictimPageNum(frameVictimTrackerLRU.get(i).getVictimPageNum());
+            for (int i = 0; i < frameVictimTrackerLRU_RAM.size(); i++){
+                if (frameVictimTrackerLRU_RAM.get(i).getVictimPID() == info.getPid() && frameVictimTrackerLRU_RAM.get(i).getVictimPageNum() == info.getPageNum(addr)){
+                    mostRecentFrame = new FrameTracker(frameVictimTrackerLRU_RAM.get(i).getFrame());
+                    mostRecentFrame.setVictimPID(frameVictimTrackerLRU_RAM.get(i).getVictimPID());
+                    mostRecentFrame.setVictimPageNum(frameVictimTrackerLRU_RAM.get(i).getVictimPageNum());
 
-                    frameVictimTrackerLRU.remove(i);
-                    frameVictimTrackerLRU.add(0, mostRecentFrame);
+                    frameVictimTrackerLRU_RAM.remove(i);
+                    frameVictimTrackerLRU_RAM.add(0, mostRecentFrame);
                     break;
                 }
+            }
+        }
+
+        //Housekeeping to ensure that only processes still being held in SimHDD are being stored in "frameVictimTrackerLRU_HDD"
+        for (int i = 0; i < frameVictimTrackerLRU_HDD.size(); i++){
+            if (SimHDD.getInstance().find(frameVictimTrackerLRU_HDD.get(i).getVictimPID(), frameVictimTrackerLRU_HDD.get(i).getVictimPageNum()) == null){
+                frameVictimTrackerLRU_HDD.remove(i);
             }
         }
     }
@@ -79,29 +117,58 @@ public class VirtualMemoryManager implements MemoryManager{
     public void readRequest(SimProcessInfo info, int addr){
         //Check to see if item being requested to be READ FROM is currently in memory
         if (SimRAM.getInstance().find(info.getPid(), info.getPageNum(addr)) == null){
+            for (int i = 0; i<frameVictimTrackerLRU_HDD.size(); i++){
+                if (frameVictimTrackerLRU_HDD.get(i).getVictimPID() == info.getPid() && frameVictimTrackerLRU_HDD.get(i).getVictimPageNum() == info.getPageNum(addr)){
+                    SimHDD.getInstance().free(frameVictimTrackerLRU_HDD.get(i).getFrame());
+                    frameVictimTrackerLRU_HDD.remove(i);
+                    break;
+                }
+            }
+
             //If page is NOT currently in memory and Page Fault DOESN'T occur, add the given page to the next available frame
             if (SimRAM.getInstance().nextFree() != -1){
                 nextPage = new FrameTracker(SimRAM.getInstance().nextFree());
                 nextPage.setVictimPID(info.getPid());
                 nextPage.setVictimPageNum(info.getPageNum(addr));
 
-                frameVictimTrackerLRU.add(0, nextPage);
+                frameVictimTrackerLRU_RAM.add(0, nextPage);
                 SimRAM.getInstance().store(info.getPage(addr), SimRAM.getInstance().nextFree());
             }
 
             //If page is NOT currently in memory and Page Fault OCCURS, determine which frame will be the victim, check whether the page within that frame has been modified, and then overwrite the frame after writing the changes out to SimHDD as necessary
             else if (SimRAM.getInstance().nextFree() == -1) {
-                FrameTracker victim = new FrameTracker(frameVictimTrackerLRU.get(frameVictimTrackerLRU.size() - 1).getFrame());
+                FrameTracker victim = new FrameTracker(frameVictimTrackerLRU_RAM.get(frameVictimTrackerLRU_RAM.size() - 1).getFrame());
+
+                //Store the frame's contents in specific slot within SimHDD in order to prevent a potentially endless number of blocks being generated in SimHDD
                 if(SimRAM.getInstance().get(victim.getFrame()).isChanged() == true){
-                    SimHDD.getInstance().addStoreBlock(SimRAM.getInstance().get(victim.getFrame()));
+//                    if (SimHDD.getInstance().find(victim.getVictimPID(), victim.getFrame()) == null){
+                    if (SimHDD.getInstance().nextFree() == -1){
+                        SimHDD.getInstance().addBlock();
+
+                        FrameTracker oldVictimFrame = new FrameTracker(SimHDD.getInstance().nextFree());
+                        frameVictimTrackerLRU_HDD.add(oldVictimFrame);
+
+                        SimHDD.getInstance().store(SimRAM.getInstance().get(victim.getFrame()), SimHDD.getInstance().nextFree());
+                    }
+                    else {
+                        FrameTracker oldVictimFrame = new FrameTracker(SimHDD.getInstance().nextFree());
+                        frameVictimTrackerLRU_HDD.add(oldVictimFrame);
+
+                        SimHDD.getInstance().store(SimRAM.getInstance().get(victim.getFrame()), SimHDD.getInstance().nextFree());
+                    }
+//                    }
+//                    else{
+//                    }
+
+////                    SimHDD.getInstance().addStoreBlock(SimRAM.getInstance().get(victim.getFrame()));
                 }
                 SimRAM.getInstance().free(victim.getFrame());
                 SimRAM.getInstance().store(info.getPage(addr), victim.getFrame());
 
                 //Modify page fault frame tracker so that frame being read from will now be the most recently used frame (and last to be chosen for Victim Selection)
-                for(int i = 0; i < frameVictimTrackerLRU.size(); i++) {
-                    if(frameVictimTrackerLRU.get(i).getVictimPID() == victim.getVictimPID() && frameVictimTrackerLRU.get(i).getVictimPageNum() == victim.getVictimPageNum()){
-                        frameVictimTrackerLRU.remove(i);
+                for(int i = 0; i < frameVictimTrackerLRU_RAM.size(); i++) {
+                    if(frameVictimTrackerLRU_RAM.get(i).getVictimPID() == victim.getVictimPID() && frameVictimTrackerLRU_RAM.get(i).getVictimPageNum() == victim.getVictimPageNum()){
+                        frameVictimTrackerLRU_RAM.remove(i);
                         break;
                     }
                 }
@@ -109,20 +176,26 @@ public class VirtualMemoryManager implements MemoryManager{
                 nextPage.setVictimPID(info.getPid());
                 nextPage.setVictimPageNum(info.getPageNum(addr));
 
-                frameVictimTrackerLRU.add(0, nextPage);
+                frameVictimTrackerLRU_RAM.add(0, nextPage);
             }
         }
 
-        //If Page IS currently in Memory, find current frame in SimRAM being used and bring it to the top of the "frameVictimTracjerLRU" arraylist so that it won't be immediately selected as the next victim        else{
-        for (int i = 0; i < frameVictimTrackerLRU.size(); i++){
-            if (frameVictimTrackerLRU.get(i).getVictimPID() == info.getPid() && frameVictimTrackerLRU.get(i).getVictimPageNum() == info.getPageNum(addr)){
-                mostRecentFrame = new FrameTracker(frameVictimTrackerLRU.get(i).getFrame());
-                mostRecentFrame.setVictimPID(frameVictimTrackerLRU.get(i).getVictimPID());
-                mostRecentFrame.setVictimPageNum(frameVictimTrackerLRU.get(i).getVictimPageNum());
+        //If Page IS currently in Memory, find current frame in SimRAM being used and bring it to the top of the "frameVictimTracjerLRU" arraylist so that it won't be immediately selected as the next victim
+        for (int i = 0; i < frameVictimTrackerLRU_RAM.size(); i++){
+            if (frameVictimTrackerLRU_RAM.get(i).getVictimPID() == info.getPid() && frameVictimTrackerLRU_RAM.get(i).getVictimPageNum() == info.getPageNum(addr)){
+                mostRecentFrame = new FrameTracker(frameVictimTrackerLRU_RAM.get(i).getFrame());
+                mostRecentFrame.setVictimPID(frameVictimTrackerLRU_RAM.get(i).getVictimPID());
+                mostRecentFrame.setVictimPageNum(frameVictimTrackerLRU_RAM.get(i).getVictimPageNum());
 
-                frameVictimTrackerLRU.remove(i);
-                frameVictimTrackerLRU.add(0, mostRecentFrame);
+                frameVictimTrackerLRU_RAM.remove(i);
+                frameVictimTrackerLRU_RAM.add(0, mostRecentFrame);
                 break;
+            }
+        }
+        //Housekeeping to ensure that only processes still being held in SimHDD are being stored in "frameVictimTrackerLRU_HDD"
+        for (int i = 0; i < frameVictimTrackerLRU_HDD.size(); i++){
+            if (SimHDD.getInstance().find(frameVictimTrackerLRU_HDD.get(i).getVictimPID(), frameVictimTrackerLRU_HDD.get(i).getVictimPageNum()) == null){
+                frameVictimTrackerLRU_HDD.remove(i);
             }
         }
     }
